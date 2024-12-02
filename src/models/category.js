9a00c497ef07f49687d0862ced238605bdeb8133
Ashwani <ashwanikumar13628@gmail.com>
2024-12-02 17:23:00 +0530

    com

--- src/models/category.js
+++ src/models/category.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const categorySchema=new mongoose.Schema({
     name:{type:String,required:true},
-    name:{type:String,required:true},
     image:{type:String,required:true},
     
 })
